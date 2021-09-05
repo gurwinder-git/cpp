4a8432c620493db231cd57ef06e6bd501f9ae102
Gurwinder Singh <guri60343@gmail.com>
2021-09-05 13:14:55 +0530

    add number using linked list

--- addingTwoLinkedList.cpp
+++ addingTwoLinkedList.cpp
@@ -12,15 +12,15 @@ struct node *addTwoNumbers(struct node *l1, struct node *l2);
 int main()
 {
     struct node n3;
-    n3.data = 4;
+    n3.data = 0;
     n3.next = NULL;
 
     struct node n2;
-    n2.data = 3;
+    n2.data = 0;
     n2.next = &n3;
 
     struct node n1;
-    n1.data = 5;
+    n1.data = 9;
     n1.next = &n2;
 
     struct node *l1 = &n1;
@@ -36,7 +36,7 @@ int main()
     n6.next = &n7;
 
     struct node n5;
-    n5.data = 8;
+    n5.data = 3;
     n5.next = &n6;
 
     struct node n4;
@@ -45,7 +45,13 @@ int main()
 
     struct node *l2 = &n4;
 
-    addTwoNumbers(l1, l2);
+    struct node *l3_head = addTwoNumbers(l1, l2);
+
+    while (l3_head)
+    {
+        cout << l3_head->data << "  ";
+        l3_head = l3_head->next;
+    }
 
     return 0;
 }
@@ -55,23 +61,50 @@ struct node *addTwoNumbers(struct node *l1, struct node *l2)
     int addition = 0;
     int remander = 0;
 
+    struct node *l3_head = (struct node *)malloc(sizeof(struct node));
+    struct node *l3_tail = l3_head;
+    struct node *prev_to_tail = NULL;
+
     while (l1 != NULL || l2 != NULL)
     {
         if (l1 != NULL)
         {
             addition = l1->data;
             l1 = l1->next;
-        }
+        };
         if (l2 != NULL)
         {
             addition = addition + l2->data;
             l2 = l2->next;
-        }
+        };
 
-        cout << (addition + remander) % 10 << ',';
+        // cout << (addition + remander) % 10 << ',';
+
+        if (l3_head == l3_tail)
+        {
+            l3_tail->data = ((addition + remander) % 10);
+            l3_tail->next = NULL;
+            prev_to_tail = l3_tail;
+            l3_tail = (struct node *)malloc(sizeof(struct node));
+        }
+        else
+        {
+            l3_tail->data = ((addition + remander) % 10);
+            l3_tail->next = NULL;
+            prev_to_tail->next = l3_tail;
+            prev_to_tail = l3_tail;
+            l3_tail = (struct node *)malloc(sizeof(struct node));
+        };
 
         remander = (addition + remander) / 10;
         addition = 0;
-    }
-    cout << remander;
+    };
+    // cout << remander;
+    if (remander > 0)
+    {
+        l3_tail->data = remander;
+        l3_tail->next = NULL;
+        prev_to_tail->next = l3_tail;
+    };
+    return l3_head;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,2 +1,17 @@
-n % 10;
-        // addition = addition + remander;
+(l3_head == l3_tail)
+        // {
+        //     cout << "==" << endl;
+        //     l3_tail->data = ((addition + remander) % 10);
+        //     l3_tail->next = NULL;
+        //     prev_to_tail = l3_tail;
+        //     l3_tail = (struct node *)malloc(sizeof(struct node));
+        // }
+        // else
+        // {
+        //     cout << "!=" << endl;
+        //     l3_tail->data = ((addition + remander) % 10);
+        //     l3_tail->next = NULL;
+        //     prev_to_tail->next = l3_tail;
+        //     prev_to_tail = l3_tail;
+        //     l3_tail = (struct node *)malloc(sizeof(struct node));
+        // }
