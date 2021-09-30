56fe8f1447adf811d9391cc6a24c797690c53834
Gurwinder Singh <guri60343@gmail.com>
2021-09-30 18:34:21 +0530

    maxProfit

---
+++ maxProfit.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int maxProfit(vector<int> &prices);
+int main()
+{
+    vector<int> prices = {7, 1, 2, 3, 4, 6, 3};
+    cout << maxProfit(prices);
+}
+int maxProfit(vector<int> &prices)
+{
+    int maxProfit = 0;
+    int minPrice = prices[0];
+
+    for (int i = 1; i < prices.size(); i++)
+    {
+        if (prices[i] - minPrice > maxProfit)
+            maxProfit = prices[i] - minPrice;
+
+        minPrice = min(minPrice, prices[i]);
+    }
+    return maxProfit;
+}
