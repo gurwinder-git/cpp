48adecfc8f315511d62871f583bedeeca72804f9
Gurwinder Singh <guri60343@gmail.com>
2021-10-25 21:38:26 +0530

    partion equal sum updated

--- paintingFenceProblem.cpp
+++ paintingFenceProblem.cpp
@@ -15,15 +15,17 @@ long long int countWays(int n, int k)
     if (n == 1)
         return k;
 
-    long long int same = k * 1;
-    long long int diff = k * (k - 1);
-    long long int total = same + diff;
+    long long mod = 1e9 + 7;
+
+    long long same = k * 1;
+    long long diff = k * (k - 1);
+    long long total = same + diff;
 
     for (int i = 3; i <= n; i++)
     {
         same = diff;
-        diff = total * (k - 1);
-        total = same + diff;
+        diff = (total * (k - 1)) % mod;
+        total = (same + diff) % mod;
     }
     return total;
 }
