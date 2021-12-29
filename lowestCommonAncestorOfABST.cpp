4a6be92d1f261c41d988cc3c714bbe041a40d16f
Gurwinder Singh <guri60343@gmail.com>
2021-12-29 15:17:35 +0530

    lowestCommonAncestorOfABST

---
+++ lowestCommonAncestorOfABST.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        if (root->val < p->val && root->val < q->val)
+            return lowestCommonAncestor(root->right, p, q);
+
+        else if (root->val > p->val && root->val > q->val)
+            return lowestCommonAncestor(root->left, p, q);
+        else
+            return root;
+    }
+};
+
+int main()
+{
+    TreeNode *root = new TreeNode(5);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(7);
+
+    root->left->left = new TreeNode(1);
+    root->left->right = new TreeNode(3);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(8);
+
+    Solution sol;
+
+    cout << sol.lowestCommonAncestor(root, root->right->left, root->right->right)->val;
+
+    return 0;
+}
