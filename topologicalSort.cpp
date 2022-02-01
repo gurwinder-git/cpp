2bc9c64c6e13a4b769f4b5e3bbeedef61ec44f9d
Gurwinder Singh <guri60343@gmail.com>
2022-02-01 19:07:57 +0530

    topological sort graph

--- topologicalSort.cpp
+++ topologicalSort.cpp
@@ -1,8 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
 class Solution
 {
 public:
     stack<int> stk;
-    void topoSort(vector<int> adj[], int src, vector<bool> &visited)
+
+    void topoSort_(vector<int> adj[], int src, vector<bool> &visited)
     {
         visited[src] = true;
 
@@ -10,23 +16,22 @@ public:
         {
             if (visited[adj[src][i]] == false)
             {
-                topoSort(adj, adj[src][i], visited);
+                topoSort_(adj, adj[src][i], visited);
             }
         }
-
         stk.push(src);
     }
 
     vector<int> topoSort(int V, vector<int> adj[])
     {
-
         vector<bool> visited(V, false);
 
-        for (int i; i < V; i++)
+        for (int i = 0; i < V; i++)
         {
             if (visited[i] == false)
             {
                 visited[i] = true;
+                topoSort_(adj, i, visited);
             }
         }
 
@@ -42,3 +47,24 @@ public:
         return res;
     }
 };
+
+int main()
+{
+    vector<int> adj[6] = {
+        vector<int>({1, 2}), // 0
+        vector<int>({4}),    // 1
+        vector<int>({3}),    // 2
+        vector<int>({1}),    // 3
+        vector<int>({5}),    // 4
+        vector<int>({})      // 5
+    };
+
+    Solution sol;
+
+    vector<int> res = sol.topoSort(6, adj);
+
+    for (int i : res)
+        cout << i << "  ";
+
+    return 0;
+}
