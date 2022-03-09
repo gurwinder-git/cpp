970900939060ceec1f3460837b66ee5df67784ab
Gurwinder Singh <guri60343@gmail.com>
2022-03-09 11:58:13 +0530

    letter case permutation

---
+++ letterCasePermutation.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    void dfs(string &s, int idx, vector<string> &ans)
+    {
+        if (idx >= s.length())
+        {
+            ans.push_back(s);
+
+            return;
+        }
+
+        if (!isalpha(s[idx]))
+        {
+            dfs(s, idx + 1, ans);
+        }
+        else
+        {
+            dfs(s, idx + 1, ans);
+
+            s[idx] = toupper(s[idx]);
+            dfs(s, idx + 1, ans);
+            s[idx] = tolower(s[idx]);
+        }
+    }
+    vector<string> letterCasePermutation(string s)
+    {
+        vector<string> ans;
+
+        for (int i = 0; i < s.length(); i++)
+            if (isalpha(s[i]))
+                s[i] = (char)tolower(s[i]);
+
+        dfs(s, 0, ans);
+        return ans;
+    }
+};
+
+int main()
+{
+    string s = "a1b2";
+    Solution sol;
+    vector<string> ans = sol.letterCasePermutation(s);
+
+    for (string &str : ans)
+    {
+        cout << str << "  ";
+    }
+
+    return 0;
+}
